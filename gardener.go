cb3b96140ca7b30287dfb46cf72aa5bbdbe776c1
ghust1995 <gustavo.ceci95@gmail.com>
2015-07-05 19:24:08 -0300

    COMMIT TO TEST BUILD GARDENER NOT WORKING

--- gardener.go
+++ gardener.go
@@ -1,7 +1,7 @@
 package huffman
 
 import(
-  "priorityQueue"
+  "github.com/caiquelira/huffman/priorityQueue"
   "tree"
   "container/heap"
 )
@@ -10,6 +10,7 @@ import(
 func harvest(freqMap map[string]int) *tree.Node {
   // Primeiro a gente cria a nossa priorityqueue a partir do dicionario de frequencias
   pq := make(priorityQueue, len(freqMap))
-  for
+  
+  return nil
 
 }
