65a91ebb7eed1be76c961824ea53b52301425f17
ghust1995 <gustavo.ceci95@gmail.com>
2015-07-05 21:23:00 -0300

    Added gardener test

---
+++ gardener_test.go
@@ -0,0 +1,26 @@
+package huffman
+
+import(
+  "fmt"
+  "github.com/caiquelira/huffman/tree"
+)
+
+func ExampleGardener() {
+  freqMap := map[string]int{
+    'a': 1, 'b': 2, 'c': 3, 'd': 4,
+  }
+
+  t := harvest(freqMap)
+
+  fmt.Print(t)
+
+  // Output:
+  // ""
+  //   "d"
+  //   ""
+  //     "c"
+  //     ""
+  //       "a"
+  //       "b"
+
+}
