7325c11869ab0defb1026d8164b1acab8e606964
ghust1995 <gustavo.ceci95@gmail.com>
2015-07-05 22:26:50 -0300

    FMT and minor fixes

--- gardener.go
+++ gardener.go
@@ -1,36 +1,36 @@
 package huffman
 
-import(
-  "github.com/caiquelira/huffman/huffmanHeap"
-  "github.com/caiquelira/huffman/tree"
-  "container/heap"
-  "errors"
+import (
+	"container/heap"
+	"errors"
+	"github.com/caiquelira/huffman/huffmanHeap"
+	"github.com/caiquelira/huffman/tree"
 )
 
 // Retorna a arvore
-func harvest(freqMap map[string]int) *tree.Node {
-  // Primeiro a gente cria a nossa priorityqueue a partir do dicionario de frequencias
-  hh := huffmanHeap.New(freqMap)
+func Harvest(freqMap map[string]int) *tree.Node {
+	// Primeiro a gente cria a nossa priorityqueue a partir do dicionario de frequencias
+	hh := huffmanHeap.New(freqMap)
 
-  for {
-    // Caso a heap contenha um unico elemento retornamos ela
-    if hh.Len() == 1 {
-      return heap.Pop(&hh).(*huffmanHeap.Item).Node
-    }
+	for {
+		// Caso a heap contenha um unico elemento retornamos ela
+		if hh.Len() == 1 {
+			return heap.Pop(&hh).(*huffmanHeap.Item).Node
+		}
 
-    // Caso contrario pegamos os dois elementos do topo
-    r, ok1 := heap.Pop(&hh).(*huffmanHeap.Item)
-    l, ok2 := heap.Pop(&hh).(*huffmanHeap.Item)
-    if !ok1 || !ok2 {
-      panic(errors.New("Element was not of type huffmanHeap.Item"))
-    }
+		// Caso contrario pegamos os dois elementos do topo
+		r, ok1 := heap.Pop(&hh).(*huffmanHeap.Item)
+		l, ok2 := heap.Pop(&hh).(*huffmanHeap.Item)
+		if !ok1 || !ok2 {
+			panic(errors.New("Element was not of type huffmanHeap.Item"))
+		}
 
-    //E criamos uma "arvore" intermediaria com eles
-    newItem := &huffmanHeap.Item{
-               Node: tree.New("", r.Node, l.Node),
-               Frequency: r.Frequency + l.Frequency,
-    }
-    // Adicionando em seguida ao
-    heap.Push(&hh, newItem)
-  }
+		//E criamos uma "arvore" intermediaria com eles
+		newItem := &huffmanHeap.Item{
+			Node:      tree.New("", r.Node, l.Node),
+			Frequency: r.Frequency + l.Frequency,
+		}
+		// Adicionando em seguida ao
+		heap.Push(&hh, newItem)
+	}
 }
